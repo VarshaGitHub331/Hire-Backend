6c6d34cdeff8138cc4a1ae39890295177f011ef8
Varsha Sudarsan <varsha.sudarsan221@gmail.com>
2024-12-17 20:17:46 +0530

    Edit gig

--- controllers/GigControllers.js
+++ controllers/GigControllers.js
@@ -80,5 +80,125 @@ const FetchGigs = async (req, res, next) => {
     next(e); // Pass any error to the next middleware
   }
 };
+const EditGig = async (req, res, next) => {
+  const { gig_id, gigTitle, budget, category, skillDetails, features } =
+    req.body;
+  console.log(req.body);
+  try {
+    // Fetch category_id for the given category name
+    const categoryData = await Category.findOne({
+      where: { category_name: category },
+      raw: true, // raw: true returns the data as plain object
+    });
 
-module.exports = { FetchGigs };
+    if (!categoryData) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    const category_id = categoryData.category_id;
+
+    // Update Gig category
+    await Gig_Categories.update(
+      { category_id }, // Update the category_id field
+      { where: { gig_id } } // Condition to find the relevant gig
+    );
+
+    // Update the Gig table
+    await Gigs.update(
+      { title: gigTitle, budget, category_id, features },
+      { where: { gig_id } }
+    );
+
+    for (skillDetail of skillDetails) {
+      if (skillDetail.status == "updated") {
+        const updatedFrom = skillDetail.updatedFrom;
+        const updatedTo = skillDetail.updatedTo;
+        const updatedFrom_id = await Skills.findOne(
+          {
+            where: { skill_name: updatedFrom },
+          },
+          { raw: true }
+        );
+        const updatedTo_id = await Skills.findOne(
+          {
+            where: { skill_name: updatedTo },
+          },
+          { raw: true }
+        );
+        await Gig_Skills.destroy({
+          where: { gig_id, skill_id: updatedFrom_id.skill_id },
+        });
+        await Gig_Skills.create({
+          gig_id,
+          skill_id: updatedTo_id.skill_id,
+        });
+      } else {
+        const addedSkill = skillDetail.addedSkill;
+        await Gig_Skills.create({ gig_id, skill_id: addedSkill });
+      }
+    }
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+const FetchGig = async (req, res, next) => {
+  const { user_id, gig_id } = req.body;
+  const results = [];
+  try {
+    const gigResults = await Gigs.findAll({
+      include: [
+        {
+          model: Gig_Categories,
+          where: {
+            gig_id: gig_id,
+          },
+          attributes: ["category_id"],
+        },
+      ],
+      raw: true,
+    });
+    console.log(gigResults);
+    for (let gig of gigResults) {
+      const skillResult = await Gigs.findAll({
+        include: [
+          {
+            model: Skills,
+            attributes: [
+              [
+                sequelize.fn(
+                  "GROUP_CONCAT",
+                  sequelize.col("Skills.skill_name")
+                ),
+                "skill_names",
+              ],
+            ],
+            through: {
+              attributes: [], // No additional attributes from the join table
+            },
+          },
+        ],
+        where: { gig_id: gig.gig_id },
+        raw: true,
+        group: ["Gigs.gig_id"], // Ensure grouping by gig_id
+        attributes: [],
+      });
+      const categoryName = await Category.findOne({
+        where: { category_id: gig["Gig_Category.category_id"] },
+        attributes: ["category_name"],
+        raw: true,
+      });
+      console.log(skillResult);
+      console.log(categoryName.category_name);
+      gig.category_name = categoryName.category_name;
+      gig.skills_names = skillResult[0]["Skills.skill_names"];
+      results.push(gig);
+    }
+    console.log(results);
+    res.status(200).json(results);
+  } catch (e) {
+    next(e); // Pass any error to the next middleware
+  }
+};
+
+module.exports = { FetchGigs, EditGig, FetchGig };
--- routes/gigs.js
+++ routes/gigs.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const gigRouter = express.Router();
-const { FetchGigs } = require("../controllers/GigControllers");
+const {
+  FetchGigs,
+  EditGig,
+  FetchGig,
+} = require("../controllers/GigControllers");
 const WrapAsync = require("../utils/WrapAsync.js");
 gigRouter.get("/getGigs", WrapAsync(FetchGigs));
-
+gigRouter.put("/editGig", WrapAsync(EditGig), WrapAsync(FetchGig));
+gigRouter.get("/fetchGig", WrapAsync(FetchGig));
 module.exports = gigRouter;
